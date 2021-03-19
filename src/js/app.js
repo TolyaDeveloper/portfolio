3c69f84b5db5a7c7a7d9a8dbb6be03aadccd6843
Anatoliy <kabantolyan@gmail.com>
2021-03-19 16:52:41 +0200

    sidenav interactivity

--- src/js/app.js
+++ src/js/app.js
@@ -1,2 +1,3 @@
 // import './modules/scrollbar';
 import './modules/forms';
+import './modules/sidenav';
---
+++ src/js/modules/sidenav.js
@@ -0,0 +1,33 @@
+const sidenav = document.querySelector('.sidenav');
+const burger = document.querySelector('.hamburger');
+const closeSidenavBtn = sidenav.querySelector('.close-sidenav');
+
+const closeSidenav = (selector) => {
+  selector.classList.remove('active');
+  document.body.classList.remove('no-scroll');
+};
+
+const openSidenav = (selector) => {
+  selector.classList.add('active');
+  document.body.classList.add('no-scroll');
+};
+
+burger.addEventListener('click', () => {
+  openSidenav(sidenav);
+});
+
+closeSidenavBtn.addEventListener('click', () => {
+  closeSidenav(sidenav);
+});
+
+window.addEventListener('keyup', (e) => {
+  if (e.code === 'Escape' && sidenav.matches('.active')) {
+    closeSidenav(sidenav);
+  }
+});
+
+sidenav.addEventListener('click', (e) => {
+  if (e.target.matches('.sidenav__overlay')) {
+    closeSidenav(sidenav);
+  }
+});
